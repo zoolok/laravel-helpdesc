a600df8f1b94765fa93e16cee7594ae483169dbe
zoolok <mityasazonow@yandex.ru>
2022-09-09 10:07:45 +0300

    первые миграции

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\MainController;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[MainController::class, 'index']);
+
+Route::post('/', function () {
+   return response()->json(['test_response' => 'ok'],201);
 });
